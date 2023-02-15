86aeb0c62c15789c53bebfd5fcd1cd54e5c676c4
ke-leee <lkee0722@naver.com>
2023-02-15 15:51:08 +0900

    set up2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import ToDoList from "./ToDoList";
-import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <ToDoList />
-      </ThemeProvider>
+      <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
